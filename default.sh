e19d995bd9f7410e807deec82cc18dc69d148a3f
rami-alfish <ramizx1995@gmail.com>
2024-11-04 20:32:18 +0100

    Remove embedded repository 'torte' from the index

---
+++ default.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+# The following line uses curl to reproducibly install and run the specified revision of torte.
+# Alternatively, torte can be installed manually (see https://github.com/ekuiter/torte).
+# In that case, make sure to check out the correct revision manually and run ./torte.sh <this-file>.
+TORTE_REVISION=main; [[ $TOOL != torte ]] && builtin source /dev/stdin <<<"$(curl -fsSL https://raw.githubusercontent.com/ekuiter/torte/$TORTE_REVISION/torte.sh)" "$@"
+
+# This experiment extracts, transforms, and analyzes a single feature model.
+# It serves as a demo and integration test for torte and also returns some common statistics of the model.
+
+TIMEOUT=10
+
+experiment-subjects() {
+    add-busybox-kconfig-history --from 1_36_0 --to 1_36_1
+}
+
+experiment-stages() {
+    clone-systems
+    read-statistics
+    extract-kconfig-models
+    
+    transform-models-with-featjar --transformer model_to_xml_featureide --output-extension xml --timeout "$TIMEOUT"
+    transform-models-with-featjar --transformer model_to_uvl_featureide --output-extension uvl --timeout "$TIMEOUT"
+    transform-models-into-dimacs --timeout "$TIMEOUT"
+    
+    draw-community-structure --timeout "$TIMEOUT"
+    compute-backbone-dimacs-with-cadiback --timeout "$TIMEOUT"
+    compute-unconstrained-features --timeout "$TIMEOUT"
+    compute-backbone-features --timeout "$TIMEOUT"
+    solve-satisfiable --timeout "$TIMEOUT"
+    solve-model-count --timeout "$TIMEOUT"
+
+    join-into kconfig dimacs
+    join-into dimacs community-structure
+    join-into dimacs solve_satisfiable
+    join-into dimacs solve_model-count
+
+    log-output-field read-statistics source_lines_of_code
+    log-output-field kconfig model-features
+    log-output-field dimacs dimacs-variables
+    log-output-field solve_satisfiable satisfiable
+    log-output-field solve_model-count model-count
+    run-notebook --file experiments/default.ipynb
+}
