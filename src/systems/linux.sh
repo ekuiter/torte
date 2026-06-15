cc06c35f4c3cb3a73df3c90afeac84679cf5a296
Elias Kuiter <info@elias-kuiter.de>
2026-06-15 17:23:52 +0200

    revised Linux system

--- src/systems/linux.sh
+++ src/systems/linux.sh
@@ -3,17 +3,22 @@
 LINUX_URL=https://github.com/torvalds/linux
 LINUX_URL_FORK=https://github.com/ekuiter/torte-linux
 
+# Linux is a fairly complicated system, also there is no predefined define-system here
+# instead all the functions are handcrafted (e.g., to account for CPU architectures)
+
+# by default, we work on a rewritten (i.e., transformed) repository of Linux that has some useful properties
 add-linux-system(transform...) {
     if is-array-empty transform; then
         transform=(filter-case-insensitive tag-old-releases)
     fi
+    add-hook-step post-clone-hook post-clone-hook-linux
     add-hook-step kconfig-post-checkout-hook kconfig-post-checkout-hook-linux
     add-hook-step kconfig-pre-binding-hook kconfig-pre-binding-hook-linux
     add-hook-step kconfig-pre-hierarchy-hook kconfig-pre-hierarchy-hook-linux
-    add-hook-step post-clone-hook post-clone-hook-linux
     add-system --system linux --url "$LINUX_URL" --fork-url "$LINUX_URL_FORK" --transform "${transform[@]}"
 }
 
+# after cloning, execute all transformations requested by the first call to add-linux-system
 post-clone-hook-linux(system, transform...) {
     if [[ $system == linux ]]; then
         if array-contains filter-case-insensitive "${transform[@]}"; then
@@ -25,14 +30,14 @@ post-clone-hook-linux(system, transform...) {
     fi
 }
 
+# we need to purge a few files from the git history, which cannot be checked out on case-insensitive file systems
+# we don't need these files anyway for feature-model extraction
+# this changes all commit hashes
+# see https://github.com/torvalds/linux/tree/v3.0/include/linux/netfilter_ipv4
+# and https://github.com/ekuiter/torte/blob/637bdaf85d8558ccb491abe725e312488d101fc9/src/systems/linux.sh
+# if you need original commit hashes, explicitly add linux without transform in the experiment:
+# add-system --system linux --url "$LINUX_URL"
 filter-case-insensitive-linux() {
-    # we need to purge a few files from the git history, which cannot be checked out on case-insensitive file systems
-    # we don't need these files anyway for feature-model extraction
-    # this changes all commit hashes
-    # see https://github.com/torvalds/linux/tree/v3.0/include/linux/netfilter_ipv4
-    # and https://github.com/ekuiter/torte/blob/637bdaf85d8558ccb491abe725e312488d101fc9/src/systems/linux.sh
-    # if you need original commit hashes, explicitly add linux without transform in the experiment:
-    # add-system --system linux --url "$LINUX_URL"
     git -C "$(input-directory)/linux" filter-repo --force --invert-paths \
         --path-glob 'include/*/xt_*' \
         --path-glob 'include/*/ipt_*' \
@@ -43,6 +48,60 @@ filter-case-insensitive-linux() {
         --path-glob '*/Z6.0+pooncelock+poonceLock+pombonce*' \
         --path-glob '*/Z6.0+pooncelock+pooncelock+pombonce*' \
         --path-glob 'Documentation/io-mapping.txt'
+}
+
+# adds old Linux revisions before the first Git tag v2.6.12
+# creates an orphaned branch and tag for each revision
+# tags all revisions between 2.5.45 and 2.6.12, as these use Kconfig
+tag-old-releases-linux() {
+    if git -C "$(input-directory)/linux" show-branch v2.6.11 2>&1 | grep -q "No revs to be shown."; then
+        git -C "$(input-directory)/linux" tag -d v2.6.11 # delete non-commit 2.6.11
+    fi
+
+    # could also tag older revisions, but none use Kconfig
+    tag-old-releases-linux-helper https://mirrors.edge.kernel.org/pub/linux/kernel/v2.5/ 2.5.45
+    tag-old-releases-linux-helper https://mirrors.edge.kernel.org/pub/linux/kernel/v2.6/ 2.6.0 2.6.12
+    # could also add more granular revisions with minor or patch level after 2.6.12, if necessary
+
+    git -C "$(input-directory)/linux" prune
+    git -C "$(input-directory)/linux" gc
+}
+
+# downloads old Linux releases from the given webpage and tags them in the Git repository
+tag-old-releases-linux-helper(base_uri, start_inclusive=, end_exclusive=) {
+    local revisions
+    revisions=$(curl -s "$base_uri" \
+        | sed 's/.*>\(.*\)<.*/\1/g' | grep .tar.gz | cut -d- -f2 | sed 's/\.tar\.gz//' | sort -V \
+        | start-at-revision "$start_inclusive" \
+        | stop-at-revision "$end_exclusive")
+    for revision in $revisions; do
+        if ! git -C "$(input-directory)/linux" tag | grep -q "^v$revision$"; then
+            log "linux@$revision" "$(echo-progress tag)"
+            local date
+            date=$(date -d "$(curl -s "$base_uri" | grep "linux-$revision.tar.gz" \
+                | cut -d'>' -f3 | tr -s ' ' | cut -d' ' -f2- | rev | cut -d' ' -f2- | rev)" +%s)
+            push "$(input-directory)"
+            rm-safe ./*.tar.gz*
+            wget -q "$base_uri/linux-$revision.tar.gz"
+            tar xzf ./*.tar.gz*
+            rm-safe ./*.tar.gz*
+            push linux
+            git reset -q --hard >/dev/null
+            git clean -q -dfx >/dev/null
+            git checkout -q --orphan "$revision" >/dev/null
+            git reset -q --hard >/dev/null
+            git clean -q -dfx >/dev/null
+            cp -R "../linux-$revision/." ./
+            git add -A >/dev/null
+            GIT_COMMITTER_DATE=$date git commit -q --date "$date" -m "v$revision" >/dev/null
+            git tag "v$revision" >/dev/null
+            pop
+            rm-safe "linux-$revision"
+            log "" "$(echo-done)"
+        else
+            log "" "$(echo-skip)"
+        fi
+    done
 }
 
 kconfig-post-checkout-hook-linux(system, revision) {
@@ -88,35 +147,6 @@ linux-tags() {
 linux-architectures(revision) {
     git -C "$(input-directory)/linux" ls-tree -rd "$revision" --name-only \
         | grep ^arch/ | cut -d/ -f2 | sort | uniq | grep -v '^um$'
-}
-
-linux-configs(revision) {
-    # match lines in all Kconfig files of the given revision that:
-    # - start with 'config' or 'menuconfig' (possibly with leading whitespace)
-    # - after which follows an alphanumeric configuration option name
-    # then format the result by removing 'config' or 'menuconfig', possible comments, and trimming any whitespace
-    # finally, ignore all lines which contain illegal characters (e.g., whitespace)
-    # note that this does not exclude the architecture um as done in other functions
-    git -C "$(input-directory)/linux" grep -E $'^[ \t]*(menu)?config[ \t]+[0-9a-zA-Z_]+' "$revision" -- '**/*Kconfig*' \
-        | awk -F: $'{OFS=","; gsub("^[ \t]*(menu)?config[ \t]+", "", $3); gsub("#.*", "", $3); gsub(/^[[:space:]]+|[[:space:]]+$/, "", $3); print "linux", $1, $2, $3}' \
-        | grep -E ',.*,.*,[0-9a-zA-Z_]+$' \
-        | sort | uniq
-}
-
-linux-config-types(revision) {
-    # similar to linux-configs, reads all configuration options, but also tries to read their types from the succeeding line
-    # note that this is less accurate than linux-configs due to the complexity of the regular expressions
-    # also, this does not exclude the architecture um as done in other functions
-    git -C "$(input-directory)/linux" grep -E -A1 $'^[ \t]*(menu)?config[ \t]+[0-9a-zA-Z_]+' "$revision" -- '**/*Kconfig*' \
-        | perl -pe 's/[ \t]*(menu)?config[ \t]+([0-9a-zA-Z_]+).*/$2/' \
-        | perl -pe 's/\n/&&&/g' \
-        | perl -pe 's/&&&--&&&/\n/g' \
-        | perl -pe 's/&&&[^:&]*?:[^:&]*?Kconfig[^:&]*?-/&&&/g' \
-        | perl -pe 's/&&&([^:&]*?:[^:&]*?Kconfig[^:&]*?:)/&&&\n$1/g' \
-        | perl -pe 's/&&&/:/g' \
-        | awk -F: $'{OFS=","; gsub(".*bool.*", "bool", $4); gsub(".*tristate.*", "tristate", $4); gsub(".*string.*", "string", $4); gsub(".*int.*", "int", $4); gsub(".*hex.*", "hex", $4); print "linux", $1, $2, $3, $4}' \
-        | grep -E ',(bool|tristate|string|int|hex)$' \
-        | sort | uniq
 }
 
 linux-attempt-grouper(file, query) {
@@ -232,141 +262,6 @@ add-linux-kconfig-tags(from=, to=, architecture=x86) {
 
 add-linux-kconfig-sample(interval, architecture=x86) {
     add-linux-kconfig-revisions "$(memoize-global git-sample-commits linux "$interval" master)" "$architecture"
-}
-
-# adds old Linux revisions before the first Git tag v2.6.12
-# creates an orphaned branch and tag for each revision
-# tags all revisions between 2.5.45 and 2.6.12, as these use Kconfig
-tag-old-releases-linux() {
-    if git -C "$(input-directory)/linux" show-branch v2.6.11 2>&1 | grep -q "No revs to be shown."; then
-        git -C "$(input-directory)/linux" tag -d v2.6.11 # delete non-commit 2.6.11
-    fi
-
-    # could also tag older revisions, but none use Kconfig
-    linux-tag-old-releases-from-tarballs https://mirrors.edge.kernel.org/pub/linux/kernel/v2.5/ 2.5.45
-    linux-tag-old-releases-from-tarballs https://mirrors.edge.kernel.org/pub/linux/kernel/v2.6/ 2.6.0 2.6.12
-    # could also add more granular revisions with minor or patch level after 2.6.12, if necessary
-
-    git -C "$(input-directory)/linux" prune
-    git -C "$(input-directory)/linux" gc
-}
-
-linux-tag-old-releases-from-tarballs(base_uri, start_inclusive=, end_exclusive=) {
-    local revisions
-    revisions=$(curl -s "$base_uri" \
-        | sed 's/.*>\(.*\)<.*/\1/g' | grep .tar.gz | cut -d- -f2 | sed 's/\.tar\.gz//' | sort -V \
-        | start-at-revision "$start_inclusive" \
-        | stop-at-revision "$end_exclusive")
-    for revision in $revisions; do
-        if ! git -C "$(input-directory)/linux" tag | grep -q "^v$revision$"; then
-            log "linux@$revision" "$(echo-progress tag)"
-            local date
-            date=$(date -d "$(curl -s "$base_uri" | grep "linux-$revision.tar.gz" \
-                | cut -d'>' -f3 | tr -s ' ' | cut -d' ' -f2- | rev | cut -d' ' -f2- | rev)" +%s)
-            push "$(input-directory)"
-            rm-safe ./*.tar.gz*
-            wget -q "$base_uri/linux-$revision.tar.gz"
-            tar xzf ./*.tar.gz*
-            rm-safe ./*.tar.gz*
-            push linux
-            git reset -q --hard >/dev/null
-            git clean -q -dfx >/dev/null
-            git checkout -q --orphan "$revision" >/dev/null
-            git reset -q --hard >/dev/null
-            git clean -q -dfx >/dev/null
-            cp -R "../linux-$revision/." ./
-            git add -A >/dev/null
-            GIT_COMMITTER_DATE=$date git commit -q --date "$date" -m "v$revision" >/dev/null
-            git tag "v$revision" >/dev/null
-            pop
-            rm-safe "linux-$revision"
-            log "" "$(echo-done)"
-        else
-            log "" "$(echo-skip)"
-        fi
-    done
-}
-
-# extracts code names of linux revisions, just because it's fun :-)
-read-linux-names() {
-    add-revision(system, revision) {
-        if [[ $system == linux ]]; then
-            log "read-linux-name: $system@$revision" "$(echo-progress read)"
-            if grep -q "^$system,$revision," "$(output-csv)"; then
-                log "" "$(echo-skip)"
-                return
-            fi
-            if [[ ! -d $(input-directory)/linux ]]; then
-                error "Linux has not been cloned yet. Please prepend a stage that clones Linux."
-            fi
-            local name
-            name=$({ git -C "$(input-directory)/linux" show "$revision:Makefile" | grep -oP "^NAME = \K.*"; } || true)
-            name=${name:-NA}
-            echo "$system,$revision,$name" >> "$(output-csv)"
-            log "" "$(echo-done)"
-        fi
-    }
-
-    echo system,revision,name > "$(output-csv)"
-    experiment-systems
-}
-
-# extracts architectures of linux revisions
-read-linux-architectures() {
-    add-revision(system, revision) {
-        if [[ $system == linux ]]; then
-            log "read-linux-architectures: $system@$revision" "$(echo-progress read)"
-            if grep -q "^$system,$revision," "$(output-csv)"; then
-                log "" "$(echo-skip)"
-                return
-            fi
-            if [[ ! -d $(input-directory)/linux ]]; then
-                error "Linux has not been cloned yet. Please prepend a stage that clones Linux."
-            fi
-            local architectures
-            mapfile -t architectures < <(linux-architectures "$revision")
-            for architecture in "${architectures[@]}"; do
-                echo "$system,$revision,$architecture" >> "$(output-csv)"
-            done
-            log "" "$(echo-done)"
-        fi
-    }
-
-    echo system,revision,architecture > "$(output-csv)"
-    experiment-systems
-}
-
-# extracts configuration options of linux revisions
-read-linux-configs() {
-    add-revision(system, revision) {
-        if [[ $system == linux ]]; then
-            log "read-linux-configs: $system@$revision" "$(echo-progress read)"
-            if grep -q "^$system,$revision," "$(output-csv)"; then
-                log "" "$(echo-skip)"
-                return
-            fi
-            if [[ ! -d $(input-directory)/linux ]]; then
-                error "Linux has not been cloned yet. Please prepend a stage that clones Linux."
-            fi
-            local configs config_types
-            configs=$(mktemp)
-            config_types=$(mktemp)
-            echo system,revision,kconfig_file,config >> "$configs"
-            echo system,revision,kconfig_file,config,type >> "$config_types"
-            linux-configs "$revision" >> "$configs"
-            tail -n+2 < "$configs" >> "$(output-csv)"
-            linux-config-types "$revision" >> "$config_types"
-            if [[ ! -f $(output-file types.csv) ]]; then
-                join-tables "$configs" "$config_types" | head -n1 > "$(output-file types.csv)"
-            fi
-            join-tables "$configs" "$config_types" | tail -n+2 >> "$(output-file types.csv)"
-            log "" "$(echo-done)"
-            rm-safe "$configs" "$config_types"
-        fi
-    }
-
-    echo system,revision,kconfig_file,config > "$(output-csv)"
-    experiment-systems
 }
 
 # modifies Kconfig files before hierarchy extraction with KConfigLib
@@ -591,4 +486,117 @@ kconfig-pre-hierarchy-hook-linux(system, revision) {
             fi
         fi
     fi
+}
+
+#---
+
+# extracts code names of linux revisions, just because it's fun :-)
+read-linux-names() {
+    add-revision(system, revision) {
+        if [[ $system == linux ]]; then
+            log "read-linux-name: $system@$revision" "$(echo-progress read)"
+            if grep -q "^$system,$revision," "$(output-csv)"; then
+                log "" "$(echo-skip)"
+                return
+            fi
+            if [[ ! -d $(input-directory)/linux ]]; then
+                error "Linux has not been cloned yet. Please prepend a stage that clones Linux."
+            fi
+            local name
+            name=$({ git -C "$(input-directory)/linux" show "$revision:Makefile" | grep -oP "^NAME = \K.*"; } || true)
+            name=${name:-NA}
+            echo "$system,$revision,$name" >> "$(output-csv)"
+            log "" "$(echo-done)"
+        fi
+    }
+
+    echo system,revision,name > "$(output-csv)"
+    experiment-systems
+}
+
+# extracts architectures of linux revisions
+read-linux-architectures() {
+    add-revision(system, revision) {
+        if [[ $system == linux ]]; then
+            log "read-linux-architectures: $system@$revision" "$(echo-progress read)"
+            if grep -q "^$system,$revision," "$(output-csv)"; then
+                log "" "$(echo-skip)"
+                return
+            fi
+            if [[ ! -d $(input-directory)/linux ]]; then
+                error "Linux has not been cloned yet. Please prepend a stage that clones Linux."
+            fi
+            local architectures
+            mapfile -t architectures < <(linux-architectures "$revision")
+            for architecture in "${architectures[@]}"; do
+                echo "$system,$revision,$architecture" >> "$(output-csv)"
+            done
+            log "" "$(echo-done)"
+        fi
+    }
+
+    echo system,revision,architecture > "$(output-csv)"
+    experiment-systems
+}
+
+linux-configs(revision) {
+    # match lines in all Kconfig files of the given revision that:
+    # - start with 'config' or 'menuconfig' (possibly with leading whitespace)
+    # - after which follows an alphanumeric configuration option name
+    # then format the result by removing 'config' or 'menuconfig', possible comments, and trimming any whitespace
+    # finally, ignore all lines which contain illegal characters (e.g., whitespace)
+    # note that this does not exclude the architecture um as done in other functions
+    git -C "$(input-directory)/linux" grep -E $'^[ \t]*(menu)?config[ \t]+[0-9a-zA-Z_]+' "$revision" -- '**/*Kconfig*' \
+        | awk -F: $'{OFS=","; gsub("^[ \t]*(menu)?config[ \t]+", "", $3); gsub("#.*", "", $3); gsub(/^[[:space:]]+|[[:space:]]+$/, "", $3); print "linux", $1, $2, $3}' \
+        | grep -E ',.*,.*,[0-9a-zA-Z_]+$' \
+        | sort | uniq
+}
+
+linux-config-types(revision) {
+    # similar to linux-configs, reads all configuration options, but also tries to read their types from the succeeding line
+    # note that this is less accurate than linux-configs due to the complexity of the regular expressions
+    # also, this does not exclude the architecture um as done in other functions
+    git -C "$(input-directory)/linux" grep -E -A1 $'^[ \t]*(menu)?config[ \t]+[0-9a-zA-Z_]+' "$revision" -- '**/*Kconfig*' \
+        | perl -pe 's/[ \t]*(menu)?config[ \t]+([0-9a-zA-Z_]+).*/$2/' \
+        | perl -pe 's/\n/&&&/g' \
+        | perl -pe 's/&&&--&&&/\n/g' \
+        | perl -pe 's/&&&[^:&]*?:[^:&]*?Kconfig[^:&]*?-/&&&/g' \
+        | perl -pe 's/&&&([^:&]*?:[^:&]*?Kconfig[^:&]*?:)/&&&\n$1/g' \
+        | perl -pe 's/&&&/:/g' \
+        | awk -F: $'{OFS=","; gsub(".*bool.*", "bool", $4); gsub(".*tristate.*", "tristate", $4); gsub(".*string.*", "string", $4); gsub(".*int.*", "int", $4); gsub(".*hex.*", "hex", $4); print "linux", $1, $2, $3, $4}' \
+        | grep -E ',(bool|tristate|string|int|hex)$' \
+        | sort | uniq
+}
+
+# extracts configuration options of linux revisions
+read-linux-configs() {
+    add-revision(system, revision) {
+        if [[ $system == linux ]]; then
+            log "read-linux-configs: $system@$revision" "$(echo-progress read)"
+            if grep -q "^$system,$revision," "$(output-csv)"; then
+                log "" "$(echo-skip)"
+                return
+            fi
+            if [[ ! -d $(input-directory)/linux ]]; then
+                error "Linux has not been cloned yet. Please prepend a stage that clones Linux."
+            fi
+            local configs config_types
+            configs=$(mktemp)
+            config_types=$(mktemp)
+            echo system,revision,kconfig_file,config >> "$configs"
+            echo system,revision,kconfig_file,config,type >> "$config_types"
+            linux-configs "$revision" >> "$configs"
+            tail -n+2 < "$configs" >> "$(output-csv)"
+            linux-config-types "$revision" >> "$config_types"
+            if [[ ! -f $(output-file types.csv) ]]; then
+                join-tables "$configs" "$config_types" | head -n1 > "$(output-file types.csv)"
+            fi
+            join-tables "$configs" "$config_types" | tail -n+2 >> "$(output-file types.csv)"
+            log "" "$(echo-done)"
+            rm-safe "$configs" "$config_types"
+        fi
+    }
+
+    echo system,revision,kconfig_file,config > "$(output-csv)"
+    experiment-systems
 }
